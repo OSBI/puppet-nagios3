6e19304c8153d608fe31e55546cf992231d29cd9
Tom Barber <tom@analytical-labs.com>
2011-10-18 15:30:39 +0100

    nagios32

---
+++ manifests/classes/monitor.pp
@@ -0,0 +1,15 @@
+class nagios::monitor {
+
+	package { ["nagios3", "nagios-plugins"]: ensure => installed }
+
+	service { "nagios3":
+		ensure => running,
+		hasstatus => true,
+		enable => true,
+		subscribe => [Package["nagios3"], Package["nagios-plugins"]],
+	}
+
+
+	Nagios_host <<||>> { notify => Serivce["nagios3"]}
+	Nagios_service <<||>> { notify => Service["nagios3"]}
+}
